6205183bc0eab85d34e7f85518ba5842ba6cc58a
leo-braida <153570963+leo-braida@users.noreply.github.com>
2026-01-12 22:53:09 -0300

    Time: 4 ms (33.41%) | Memory: 61.9 MB (28.74%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -30,43 +30,9 @@ var addTwoNumbers = function(l1, l2) {
             l2 = l2.next;
         }
 
-        //carry = result / 10 | 0;
         carry = Math.floor(result/10)
         response.next = new ListNode(result % 10);
         response = response.next
     }
     return finalResponse.next;
 };
-
-/*
-
-var addTwoNumbers = function(l1, l2) {
-    let response = new ListNode();
-    let finalResponse = response;
-
-    let carry = 0;
-    let result;
-
-    while (l1 || l2 || carry) {
-        result = carry;
-
-        if (l1) {
-            result += l1.val;
-            l1 = l1.next;
-        }
-
-        if (l2) {
-            result += l2.val;
-            l2 = l2.next;
-        }
-
-        carry = result / 10 | 0;
-
-        response.next = new ListNode(result % 10);
-        response = response.next
-    }
-    return finalResponse.next;
-};
-
-
-*/
